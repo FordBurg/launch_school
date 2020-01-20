fe8f40c2cc451b722729478df9be3ef8680ae826
FordBurg <adamtreherne@gmx.co.uk>
2020-01-20 17:57:27 +0000

    input_output exercises

---
+++ introduction_to_programming/input_output/greeter1.js
@@ -0,0 +1,8 @@
+
+let rlSync = require('readline-sync');
+
+let first_name =rlSync.question('Enter your name');
+
+console.log(`Hello ${first_name}`);
+
+
---
+++ introduction_to_programming/input_output/greeter2.js
@@ -0,0 +1,10 @@
+
+let rlSync = require('readline-sync');
+
+let first_name =rlSync.question('Enter your first name');
+
+let last_name =rlSync.question('Enter your last name');
+
+console.log(`Hello ${first_name} ${last_name}`);
+
+
